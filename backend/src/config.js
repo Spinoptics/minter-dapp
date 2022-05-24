0984b1f3f843686e194656ddb0db7466e1b06ba2
SPINOPTICS <git\STUWHEELER@gmail.com>
2022-05-23 21:43:28 -0600

    rinkeby test

--- backend/src/config.js
+++ backend/src/config.js
@@ -7,22 +7,79 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "YOUR COLLECTION NAME";
-const description = "Remember to replace this description";
+const namePrefix = "Spinoptics Generative Art Blocks";
+const description = "A programmatic art project containing over 70 layers and 400 segments with differing rarities.";
 const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 300,
     layersOrder: [
-      { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "Layer0" },
+      { name: "Layer1" },     
+      { name: "Layer2" },
+      { name: "Layer3" },
+      { name: "Layer4" },
+      { name: "Layer5" },
+      { name: "Layer6" },
+      { name: "Layer7" },
+      { name: "Layer8" },
+      { name: "Layer10" },
+      { name: "Layer11" },
+      { name: "Layer12" },
+      { name: "Layer14" },
+      { name: "Layer15" },
+      { name: "Layer16" },
+      { name: "Layer18" },
+      { name: "Layer19" },
+      { name: "Layer20" },
+      { name: "Layer21" },
+      { name: "Layer22" },
+      { name: "Layer23" },
+      { name: "Layer24" },
+      { name: "Layer25" },
+      { name: "Layer26" },
+      { name: "Layer27" },
+      { name: "Layer28" },
+      { name: "Layer29" },
+      { name: "Layer30" },
+      { name: "Layer32" },
+      { name: "Layer34" },
+      { name: "Layer35" },
+      { name: "Layer37" },
+      { name: "Layer38" },
+      { name: "Layer40" },
+      { name: "Layer41" },
+      { name: "Layer43" },
+      { name: "Layer44" },
+      { name: "Layer45" },
+      { name: "Layer46" },
+      { name: "Layer47" },
+      { name: "Layer48" },      
+      { name: "Layer49" },
+      { name: "Layer51" },
+      { name: "Layer52" },
+      { name: "Layer53" },
+      { name: "Layer54" },
+      { name: "Layer55" },
+      { name: "Layer56" },
+      { name: "Layer57" },
+      { name: "Layer58" },
+      { name: "Layer60" },
+      { name: "Layer61" },
+      { name: "Layer62" },
+      { name: "Layer63" },
+      { name: "Layer64" },
+      { name: "Layer65" },
+      { name: "Layer66" },
+      { name: "Layer69" },
+      { name: "Layer70" },
+      { name: "Layer72" },
+      { name: "Layer74" },
+      { name: "Layer75" },
+      { name: "Layer77" },
+      { name: "Layer78" },
     ],
   },
 ];
@@ -32,13 +89,13 @@ const shuffleLayerConfigurations = true;
 const debugLogs = false;
 
 const format = {
-  width: 512,
-  height: 512,
+  width: 3022,
+  height: 3022,
   smoothing: false,
 };
 
 const extraMetadata = {
-  external_url: "https://codecats.xyz", // Replace with your website or remove this line if you do not have one.
+  external_url: "https://superb-kelpie-3b681d.netlify.app/", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
@@ -49,17 +106,17 @@ const LIMIT = 2; // Your API key rate limit
 const CHAIN = 'rinkeby'; // only rinkeby or polygon
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
-const CONTRACT_NAME = 'CRYPTOPUNKS';
-const CONTRACT_SYMBOL = 'CP';
+const CONTRACT_NAME = 'Spinoptics Generative Art Blocks';
+const CONTRACT_SYMBOL = 'SB';
 const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
-const OWNER_ADDRESS = 'YOUR WALLET ADDRESS HERE';
-const TREASURY_ADDRESS = 'YOUR WALLET ADDRESS HERE';
+const OWNER_ADDRESS = '0xDd6438a6cE18b6dbcdD939c4a33A4898E38ECAa6';
+const TREASURY_ADDRESS = '0xDd6438a6cE18b6dbcdD939c4a33A4898E38ECAa6';
 const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 1; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = 0.001; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-03-03T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
@@ -75,8 +132,8 @@ let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually inclu
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
 const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
-const GENERIC_DESCRIPTION = "REPLACE THIS"; // Replace with what you want the generic descriptions to say.
-const GENERIC_IMAGE = "https://ipfs.io/ipfs/QmUf9tDbkqnfHkQaMdFWSGAeXwVXWA61pFED7ypx4hcsfh"; // Replace with your generic image that will display for all NFTs pre-reveal.
+const GENERIC_DESCRIPTION = "Which Blocks will you get?"; // Replace with what you want the generic descriptions to say.
+const GENERIC_IMAGE = "https://ipfs.io/ipfs/bafybeih62kw3mvust6tkqsvqw4k4zcgq2vbqqvcvxbq5ps2wdfhap52irm"; // Replace with your generic image that will display for all NFTs pre-reveal.
 
 // Automatically set contract address if deployed using the deployContract.js script
 try {
@@ -129,7 +186,7 @@ const pixelFormat = {
 };
 
 const background = {
-  generate: true,
+  generate: false,
   brightness: "80%",
   static: false,
   default: "#000000",
