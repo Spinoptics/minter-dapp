660176a47f842e4b44ed84b3efb6bb3f64a02d68
SPINOPTICS <git\STUWHEELER@gmail.com>
2022-05-24 09:42:36 -0600

    change

--- backend/src/config.js
+++ backend/src/config.js
@@ -9,7 +9,7 @@ const network = NETWORK.eth;
 // General metadata for Ethereum
 const namePrefix = "Spinoptics Generative Art Blocks";
 const description = "A programmatic art project containing over 70 layers and 400 segments with differing rarities.";
-const baseUri = "ipfs://bafybeibnorv6ioxqrwyqebulkwdsldc6pdy5nsa6e6rmo6h3wcslgzytsu/"; // This will be replaced automatically
+const baseUri = "https://ipfs.io/ipfs/bafybeibnorv6ioxqrwyqebulkwdsldc6pdy5nsa6e6rmo6h3wcslgzytsu/"; // This will be replaced automatically
 
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
@@ -133,7 +133,7 @@ let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually inclu
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
 const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
 const GENERIC_DESCRIPTION = "Which Blocks will you get?"; // Replace with what you want the generic descriptions to say.
-const GENERIC_IMAGE = "https://ipfs.io/ipfs/bafybeibnorv6ioxqrwyqebulkwdsldc6pdy5nsa6e6rmo6h3wcslgzytsu/"; // Replace with your generic image that will display for all NFTs pre-reveal.
+const GENERIC_IMAGE = "ipfs://bafybeibnorv6ioxqrwyqebulkwdsldc6pdy5nsa6e6rmo6h3wcslgzytsu/"; // Replace with your generic image that will display for all NFTs pre-reveal.
 
 // Automatically set contract address if deployed using the deployContract.js script
 try {
